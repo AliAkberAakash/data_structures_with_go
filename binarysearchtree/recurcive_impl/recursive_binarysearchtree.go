f43d0139aaecaf1b7f1a12a6caa899daadd83d90
ALIAKBERAAKASH <ali.akber@brainstation-23.com>
2022-04-11 00:10:32 +0600

    Add binary search tree

---
+++ binarysearchtree/recurcive_impl/recursive_binarysearchtree.go
@@ -0,0 +1,43 @@
+package recurciveimpl
+
+type Node struct {
+	Data      int
+	LeftNode  *Node
+	RightNode *Node
+}
+
+func (head *Node) Insert(value int) {
+
+	if value > head.Data {
+		if head.RightNode == nil {
+			head.RightNode = &Node{
+				Data: value,
+			}
+		} else {
+			head.RightNode.Insert(value)
+		}
+	} else {
+		if head.LeftNode == nil {
+			head.LeftNode = &Node{
+				Data: value,
+			}
+		} else {
+			head.LeftNode.Insert(value)
+		}
+	}
+}
+
+func (head *Node) Search(value int) bool {
+
+	if head == nil {
+		return false
+	}
+
+	if value > head.Data {
+		return head.RightNode.Search(value)
+	} else if value < head.Data {
+		return head.LeftNode.Search(value)
+	}
+
+	return true
+}
---
+++ binarysearchtree/recurcive_impl/recursive_binaryseearchtree_test.go
@@ -0,0 +1,50 @@
+package recurciveimpl
+
+import (
+	"testing"
+)
+
+func TestInsert(t *testing.T) {
+	var tree = &Node{
+		Data: 0,
+	}
+
+	// Test left Node
+	tree.Insert(10)
+	if tree.RightNode.Data != 10 {
+		t.Error("Data was not  inserted")
+	}
+
+	// Test right Node
+	tree.Insert(-1)
+	if tree.LeftNode.Data != -1 {
+		t.Error("Data was not  inserted")
+	}
+
+}
+
+func TestSearch(t *testing.T) {
+	var tree = &Node{
+		Data: 66,
+	}
+
+	tree.Insert(10)
+	tree.Insert(100)
+	tree.Insert(85)
+	tree.Insert(52)
+	tree.Insert(-11)
+	tree.Insert(23)
+
+	// Test if exists
+	exists := tree.Search(-11)
+	if exists == false {
+		t.Error("Failed to find node")
+	}
+
+	// Test doesn't exist
+	exists = tree.Search(50)
+	if exists == true {
+		t.Error("Found non existant node")
+	}
+
+}
